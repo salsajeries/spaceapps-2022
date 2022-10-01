216c1383511d8d3d48be9faa95d3f2b77d79f654
Hayden Estes <hayden@pop-os.localdomain>
2022-10-01 16:40:53 -0500

    more noticible wheels

--- project/scripts/reaction_wheel.gd
+++ project/scripts/reaction_wheel.gd
@@ -7,15 +7,15 @@ extends KinematicBody2D
 export (String) var press_left = "null"
 export (String) var press_right = "null"
 export (int) var rotation_dir = 0 
-export (float) var rotation_speed = 0.001
-export (float) var rotation_start = 800
+export (float) var rotation_speed = 0.005
+export (float) var rotation_start = .2
 export (float) var friction_weight = 0
 export (float) var rotation_offset = 0
 # Called when the node enters the scene tree for the first time.
 
 func _physics_process(delta):
 	rotation_offset = (rotation_dir * rotation_speed * delta)
-	self.rotate(rotation_offset + rotation_start)
+	rotation += (rotation_offset + rotation_start)
 	#print(rotation)
 	if Input. is_action_pressed(press_right):
 		rotation_dir += 1  
