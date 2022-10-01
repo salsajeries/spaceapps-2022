79582c8b093e8a82076795d5f1fc490960730b1b
Hayden Estes <hayden@pop-os.localdomain>
2022-10-01 15:22:44 -0500

    space probe 3d

--- project/scripts/psp.gd
+++ project/scripts/psp.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -10,11 +9,11 @@ export (int) var acceleration = 10
 export (int) var max_speed = 500
 
 var rotation_dir : int 
-export (float) var rotation_speed = 0.3
+export (float) var rotation_speed = 0.001
 export (float) var friction_weight = 0
 func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("move_forward") - Input.get_action_strength("move_back")
-	rotation_dir = 0
+	#rotation_dir = 0
 	if Input. is_action_pressed( "strafe_right"):
 		rotation_dir += 1  
 	if Input. is_action_pressed( "strafe_left"):
@@ -30,5 +29,6 @@ func _physics_process(delta):
 		if abs(velocity.y) == 0.1:
 			velocity.y = 0
 	rotation += rotation_dir * rotation_speed * delta
+	rotation = lerp(rotation, 0, friction_weight)
 	velocity = move_and_slide(velocity)
 	#position += Vector2.RIGHT.rotated(rotation) * velocity * delta
