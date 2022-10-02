52486c42acf9ef4d9eda3ccd284ce3cafb7180a1
Hayden Estes <hayden@pop-os.localdomain>
2022-10-02 14:15:02 -0500

    button fixed

--- project/BackToTitle.gd
+++ project/BackToTitle.gd
@@ -6,9 +6,10 @@ extends Button
 # var b = "text"
 export (String) var scene_path
 
-func _on_BackToTitleButton_pressed():
-	get_tree().change_scene(scene_path)
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+
+func _on_BackToTitle_pressed():
+		get_tree().change_scene(scene_path)
