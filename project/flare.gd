4575732dd9841e0ad9dff702cf992cf94821f73a
acual-heathen <tonyyluis@hotmail.com>
2022-10-02 14:25:36 -0500

    RAVE

--- project/flare.gd
+++ project/flare.gd
@@ -1,12 +1,13 @@
 extends Sprite3D
-export(PackedScene) var flare
-
+var flare = preload("res://prefabs/particle.tscn")
+var dot
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var x = 0
 var vecP = Vector3(0,0,0)
 var vecS = Vector3(0,0,0)
+var rng = RandomNumberGenerator.new()
 export (float) var interval
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,10 +19,9 @@ func _process(delta):
 	x = x + delta
 	if x > interval:
 		var particle = flare.instance()
-		rand_range(10,50)
 		particle.linear_velocity = vecS
-		vecP = Vector3(rand_range(-.1,.1),rand_range(-.1,.1),0)
-		vecS = Vector3(0,0,rand_range(5,100))
+		vecP = Vector3(rng.randfn(0,1),rng.randfn(0,1),0)
+		vecS = Vector3(0,0,rng.randfn(27.48713057981032, 27.3992185093072))
 		particle.translate(vecP)
 		add_child(particle)
 		x = 0
